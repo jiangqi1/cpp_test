2d10961367c8da57a7c449dbac8ecc2a676cb71b
qi.jiang <qi.jiang@finisar.com>
2015-09-28 17:04:52 +0800

    Study cpp 1, check the double/long/int/char size

--- cpp_test/cpp_test/cpp_test.cpp
+++ cpp_test/cpp_test/cpp_test.cpp
@@ -4,7 +4,25 @@ using namespace std;
 // My first C++ code
 int main()
 {
-	int a, b;
-	cin >> a >> b;
+	double d;
+	long l;
+	short s;
+	int i;
+	char c;
+	
+	int size_of_int, size_of_char, size_of_short, size_of_long, size_of_double;
+
+	size_of_double = sizeof(d);
+	size_of_long = sizeof(l);
+	size_of_int = sizeof(i);
+	size_of_char = sizeof(c);
+	size_of_short = sizeof(s);
+
+	cout << "size_of_double = " << size_of_double << '\n';
+	cout << "size_of_long = " << size_of_long << '\n';
+	cout << "size_of_int = " << size_of_int << '\n';
+	cout << "size_of_short = " << size_of_short<< '\n';
+	cout << "size_of_char = " << size_of_char;
+	cin >> c;
 	return 0;
 }
